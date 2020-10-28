4e42f65732e004295064db5e5a372faf6af78ef3
Leanne Dong <jdleanne@gmail.com>
2020-10-28 15:11:37 -0400

    added some outputs for power law

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <string>
 #include <cassert>
+#include <fstream>
 
 using namespace std;
 
@@ -54,22 +55,33 @@ int main() {
     int divs = 100;
     double low = 0;
     double high = n;
-    cerr << "gamma_n(1200)=" << gamma_n(1200,n) << "\n";
+   // cerr << "gamma_n(1200)=" << gamma_n(1200,n) << "\n";
+   // std::ofstream of_beta("/home/leanne/CLionProjects/Explicit-Volterra/outputs/beta_n.txt");
+    std::ofstream of_beta("outputs/beta_n.txt");
+   // std::ofstream of_gamma("/home/leanne/CLionProjects/Explicit-Volterra/outputs/gamma_n.txt");
+    std::ofstream of_gamma("outputs/gamma_n.txt");
+    std::ofstream of_sum2("outputs/sum2.txt");
+    std::ofstream of_all("outputs/all.txt");
+
     for (int i = 0; i <=divs; ++i)
     {
         double t = low + (i/double(divs))*(high-low);
         double sum2 = 0.0;
 
         for (int j=1; j<nn+1; ++j){
-            for (double r=0; r<j-1; ++r){
+            for (double r=0; r<j-1; ++r) {
                 auto a =
-                        [&](int r /*t*/){
-                          double t = low + (r/double(divs))*(high-low);
-                          return kernel(kappa, t, theta, c, sig, base);
+                        [&](int r /*t*/) {
+                            double t = low + (r / double(divs)) * (high - low);
+                            return kernel(kappa, t, theta, c, sig, base);
                         };
-                sum2 += beta(j, r, a, t)*gamma_n(t-r, j);
-                cerr << " t= " << t << ",  beta_n(j,r,a,t)=" << beta(j, r, a, t) << ", gamma_n(t-r,j)=" << gamma_n(t-r, j)
-                     << " h_n= " << sum2 << "\n";
+                sum2 += beta(j, r, a, t) * gamma_n(t - r, j);
+                //                cerr << " t= " << t << ",  beta_n(j,r,a,t)=" << beta(j, r, a, t) << ", gamma_n(t-r,j)=" << gamma_n(t-r, j)
+                //                     << " h_n= " << sum2 << "\n";
+                of_gamma << gamma_n(t-r, j) << "\n";
+                of_beta << beta(j, r, a, t) << "\n";
+                of_sum2 << sum2 << "\n";
+                of_all << setw(10) << t << setw(20)<< gamma_n(t-r, j) << "," << setw(20) << beta(j, r, a, t) << setw(20) << sum2 << "\n";
             }
         }
         //cerr << " t= " << t  << ", gamma_n(t, n)=" << gamma_n(t, n)  <<" , h_n=" << sum2 << "\n";
